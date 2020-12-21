47c3c08881cada60213c8dc69b8d792647573a6c
fly49 <flymaipie@gmail.com>
2020-12-21 19:50:22 +0300

    init

---
+++ lib/todo_distributed.ex
@@ -0,0 +1,18 @@
+defmodule TodoDistributed do
+  @moduledoc """
+  Documentation for TodoDistributed.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TodoDistributed.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
